e73f9b99140d969a4d069f638b167b7cc87b75c5
Pablo Rubianes <pablorubianes@gmail.com>
2023-06-08 02:02:25 -0300

    logo and next update

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -1,20 +1,21 @@
 'use client'
 import { useContext } from 'react';
 import { RoomContext } from '../RoomContext/roomContextProvider';
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { getAllUsersFromRoom, updateUsers } from '@/system/firebase';
 
 
 export default function Header() {
-    const {user, setUser, room, setRoom} =  useContext(RoomContext)
+    const { user, setUser, room, setRoom } = useContext(RoomContext)
     const router = useRouter()
 
     const resetAll = async () => {
-        let loggedUsers = await getAllUsersFromRoom(room)         
+        let loggedUsers = await getAllUsersFromRoom(room)
         let newUsers = loggedUsers.filter((logged) => {
             return logged !== user
         })
-        console.log('updated '+newUsers)
+        console.log('updated ' + newUsers)
         updateUsers(newUsers, room)
 
         setUser('')
@@ -25,18 +26,23 @@ export default function Header() {
     const UsuarioLogueado = () => {
         return (
             <>
-                <div className='grid'>
-                    <h5>Hola { user }</h5>
-                    <a onClick={resetAll}>Logout</a>
-                </div>
+                <h5>Hola {user}</h5>
+                <a onClick={resetAll}>Logout</a>
             </>
         )
     }
 
     return (
         <header className='container'>
-            <h1>StoryVote {room ? ' @ '+ room : ''}</h1>
-            { user ? <UsuarioLogueado /> : ''} 
+            <div className='grid'>
+                <h1><Image
+                    src="/logo.svg"
+                    width={60}
+                    height={60}
+                    alt="Storyvote Logo"
+                />{room ? ' StoryVote @ ' + room : ''}</h1>
+            </div>
+            {user ? <UsuarioLogueado /> : ''}
         </header>
     )
 }
